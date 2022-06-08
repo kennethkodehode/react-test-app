2b5c01f44b68cf2909498ee799216b4a3bc8a9cc
Kenneth <kenneth.kodehode@gmail.com>
2022-06-08 11:27:09 +0200

    Switched from CSS to SCSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
@@ -17,7 +17,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }
